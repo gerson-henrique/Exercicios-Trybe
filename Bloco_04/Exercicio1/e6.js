2e5f9a7e1323d09bf044cef7630b2de9b2b61a31
Gero <gersonhoa@gmail.com>
2021-10-16 18:34:33 -0300

    Exercicios pt II

--- Bloco_04/Exercicio1/e6.js
+++ Bloco_04/Exercicio1/e6.js
@@ -7,8 +7,15 @@ Exemplo: bishop (bispo) -> diagonals (diagonais) */
 let piece ="BiShop";
 
 switch (piece.toLowerCase()) {
+    case "pawn":
+        console.log("O peão se moviementa para frente  1 casa (exeto no movimento de saida, aonde pode se mover duas casas) e ataca pelas diagonais ");
+        break;
     case "bishop":
-        console.log("O Bispo se moviementa livremente pela diagonal, seu ataque, tambem pela diagonal, para na proxima casa após a manobra");
+        console.log("O bispo se movimenta e ataca livremente na diagonal");
+        break;
+
+        case "tower":
+        console.log("a torre se movimenta e ataca livremente horizontal ou verticalmente");
         break;
 
     default:
---
+++ Bloco_04/Exercicio5/ex05.js
@@ -0,0 +1,27 @@
+/*  - Agora, defina um segundo objeto 
+com a mesma estrutura (as mesmas chaves) 
+do primeiro e os seguintes valores: 'Tio Patinhas',
+'Christmas on Bear Mountain, Dell's Four Color Comics #178', 
+'O último MacPatinhas', 'Sim'. Então, imprima os valores de 
+cada objeto juntos de acordo com cada uma das chaves. 
+Valor esperado no console:    */
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  }
+
+  let infoII = {
+    personagem: 'Patinhas',
+    origem: 'Tio Patinhas',
+    nota: 'Tio do Pato Donald',
+    recorrente: 'Sim'
+  }
+
+for (const key in info) {
+  
+    console.log ( info[key] + ' e ' + infoII[key]);
+        
+    }
