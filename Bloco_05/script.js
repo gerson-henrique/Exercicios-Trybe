aa8ada4ef8c4cd2aa62753ca619ddca36118b5b4
Gero <gersonhoa@gmail.com>
2021-10-20 07:29:39 -0300

    Administrador de tempo Trybe

---
+++ Bloco_05/script.js
@@ -0,0 +1,16 @@
+document.querySelector("#header-container").style.backgroundColor = "MediumSeaGreen";
+document.querySelector("#container").style.backgroundColor = "HoneyDew";
+document.querySelector(".emergency-tasks").style.backgroundColor = "Salmon";
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "Khaki";
+ let colorC = document.querySelectorAll(".emergency-tasks h3");
+ let colorB = document.querySelectorAll(".no-emergency-tasks h3");
+ 
+ 
+
+ for(let itens in colorC  ){
+colorC[itens].style.backgroundColor = "DarkOrchid";
+colorB[itens].style.backgroundColor = "Black";
+ }
+ for(let itensII in colorB  ){
+    
+     } 
