86e55dbafea713ae84d586fc25436be3fd2229b3
Gero <gersonhoa@gmail.com>
2021-10-20 06:42:10 -0300

    exercicio 3

---
+++ Bloco_05/exercicio03/script.js
@@ -0,0 +1,9 @@
+
+
+
+
+const newPara = document.querySelector(".paragraph");
+newPara.style.fontStyle = "italic";
+
+const newSub = document.querySelectorAll("h4");
+newSub[0].style.color = "green"
