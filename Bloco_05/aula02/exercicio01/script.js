90306d7d0671671e64d9c8f756e8eb654b88d12d
Gero <gersonhoa@gmail.com>
2021-10-20 15:58:01 -0300

    Parte I da aula

---
+++ Bloco_05/aula02/exercicio01/script.js
@@ -0,0 +1,15 @@
+let camel = document.getElementById("elementoOndeVoceEsta").parentNode
+camel.style.backgroundColor = "red";
+
+let dromedary = document.getElementById("primeiroFilhoDoFilho");
+dromedary.innerText = 'manda dança';
+
+let Alpaca = document.getElementById("pai").firstChild.nextElementSibling.firstChild
+
+let  lhamma  = document.getElementById("elementoOndeVoceEsta").previousSibling
+
+let  pony = document.getElementById("elementoOndeVoceEsta").nextSibling
+
+let unicorn = document.getElementById("elementoOndeVoceEsta").nextElementSibling
+
+let pegasus = document.getElementById("pai").firstChild.nextElementSibling.nextElementSibling
