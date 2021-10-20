e567962fc2d43e781e4d0d577b5897aac7e18b9a
Gero <gersonhoa@gmail.com>
2021-10-20 05:51:46 -0300

    correção dos exercicios de ontem

--- Bloco_05/exercicio01/script.js
+++ Bloco_05/exercicio01/script.js
@@ -1,6 +1,8 @@
 const paragraph = document.getElementById("paragraph");
       paragraph.style.color = "red";
 
-    document.getElementById("page-title").innerText = "Jango:Livre";
-    document.getElementById("second-paragraph").innerText = "JANGOOOOOOOOOOO OH JANGOOOOOOOOOOOOO JANGOOOOOOOOOO [DRAMATiC NOISE INTENSIFIES]";
-    document.getElementById("subtitle").innerText = "Jango: musica legendada";
+document.getElementById("page-title").innerText = "Jango Livre"
+document.getElementById("second-paragraph").innerText = "Jango era um escravo, que foi liberto, e atua como caçador de recompensas"
+document.getElementById('subtitle').innerText = 'Um filme de ação que te prende na cadeira do inicio ao fim'
+
+
