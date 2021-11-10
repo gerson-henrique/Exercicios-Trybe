67ed1d1a79b4bc36ec660067f65655a269f6ad82
Gero <gersonhoa@gmail.com>
2021-11-10 15:08:11 -0300

    aula 1

---
+++ Bloco_07/Explicação/escopoDeVariaveis2.js
@@ -0,0 +1,11 @@
+// var userName = "Isabella";
+// var userName = "Lucas";
+// console.log(userName); // Resultado: Lucas
+
+// const favoriteLanguage = "Javascript";
+// favoriteLanguage = "Python";
+// console.log(favoriteLanguage); // Erro
+
+let favoriteTechnology = "Machine learning";
+favoriteTechnology = "Facial recognition";
+console.log(favoriteTechnology); // Facial recognition
