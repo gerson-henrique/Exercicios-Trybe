7c2fe8ff3c36d52ed73b83d9e0464cb571cf50f5
Gero <gersonhoa@gmail.com>
2021-11-10 18:34:42 -0300

    parte 1

---
+++ Bloco_07/Exercicios/exercicio02.js
@@ -0,0 +1,11 @@
+
+      let oddsAndEvens = [13, 3, 4, 10, 7, 2];
+ 
+      function sortfunction(a, b){
+        return (a - b) //faz com que o array seja ordenado numericamente e de ordem crescente.
+      }
+
+     
+      oddsAndEvens.sort(sortfunction); //resultado: [1, 3, 3, 5, 7, 9, 10]
+
+      console.log(oddsAndEvens); 
